161eefdf88cd86096955e8e8176bb2101ae77fcc
hamzalhadouchi <hamzabniss16@gmail.com>
2024-12-18 19:51:22 +0100

    create sidebar with menu and profile section

--- src/ajouter.php
+++ src/ajouter.php
@@ -4,12 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Panel</title>
-</head>
-<body>
-<link href="https://fonts.googleapis.com/css?family=Open+Sans|Playfair+Display|Poppins" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Playfair+Display|Poppins" rel="stylesheet">
 <link rel="stylesheet" href="./public/style/dach.css">
 <link rel="stylesheet" href="./public/style/form.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+</head>
+<body>
+<div class="sidebar">
+    <div class="side-header">
+        <h3>M<span>odern</span></h3>
+    </div>
+    <div class="side-content">
+        <div class="profile">
+            <div class="profile-img bg-img"
+                style="background-image: url(https://intranet.youcode.ma/storage/users/profile/1049-1728486663.JPG)">
+            </div>
+            <h4>user</h4>
+        </div>
+        <div class="side-menu">
+            <ul>
+                <li><a href="./dachbord.php"><span class="las la-home"></span><small>Dashboard</small></a></li>
+                <li><a href="./ajouter.php" class="active"><span class="las la-clipboard-list"></span><small>Ajouter</small></a></li>
+                <li><a href=""><span class="las la-envelope "></span><small>Profile</small></a></li>
+                <li><a href=""><span class="las la-user-alt"></span><small>Projects</small></a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
 <script src="https://cdn.tailwindcss.com"></script>
 
 </body>

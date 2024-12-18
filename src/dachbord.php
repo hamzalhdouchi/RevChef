51af3867ffab2117c374ce2bf877cc19f2c052c3
hamzalhadouchi <hamzabniss16@gmail.com>
2024-12-18 20:01:59 +0100

    Sidebar (menu latéral)

--- src/dachbord.php
+++ src/dachbord.php
@@ -11,3 +11,32 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
+<div class="sidebar">
+    <div class="side-header">
+        <h3>M<span>odern</span></h3>
+    </div>
+
+    <div class="side-content">
+        <div class="profile">
+            <div class="profile-img bg-img" style="background-image: url(...);"></div>
+            <?php
+            if (isset($_SESSION['user'])) {
+                $now = $_SESSION['user'];
+            }
+            ?>
+            <h4><?= $now['name_utilisateur'] ?></h4>
+            <small>Director</small>
+        </div>
+
+        <div class="side-menu">
+            <ul>
+                <li><a href="" class="active"><span class="las la-home"></span><small>Dashboard</small></a></li>
+                <li><a href="./ajouter.php"><span class="las la-clipboard-list"></span><small>Menu</small></a></li>
+                <li><a href=""><span class="las la-user-alt"></span><small>Profile</small></a></li>
+                <li><a href=""><span class="las la-envelope"></span><small>Projects</small></a></li>
+                <li><a href=""><span class="las la-shopping-cart"></span><small>Orders</small></a></li>
+                <li><a href=""><span class="las la-tasks"></span><small>Tasks</small></a></li>
+            </ul>
+        </div>
+    </div>
+</div>

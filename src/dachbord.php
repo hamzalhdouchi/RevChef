7baac8ca21184fe8b2a8a3cf1f4bae8d5e6a97d6
hamzalhadouchi <hamzabniss16@gmail.com>
2024-12-18 20:03:40 +0100

    Statistiques (Cards)

--- src/dachbord.php
+++ src/dachbord.php
@@ -66,3 +66,18 @@
         <h1>Dashboard</h1>
         <small>Home / Dashboard</small>
     </div>
+    <div class="analytics">
+    <div class="card">
+        <div class="card-head">
+            <h2>107,200</h2>
+            <span class="las la-user-friends"></span>
+        </div>
+        <div class="card-progress">
+            <small>User activity this month</small>
+            <div class="card-indicator">
+                <div class="indicator one" style="width: 60%"></div>
+            </div>
+        </div>
+    </div>
+    <!-- Répétition pour chaque statistique -->
+</div>

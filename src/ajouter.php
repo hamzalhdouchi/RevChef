fea7fe0fb0958e749d258df80001972b477975d7
hamzalhadouchi <hamzabniss16@gmail.com>
2024-12-18 19:50:25 +0100

    add external fonts and styles

--- src/ajouter.php
+++ src/ajouter.php
@@ -6,5 +6,11 @@
     <title>Admin Panel</title>
 </head>
 <body>
+<link href="https://fonts.googleapis.com/css?family=Open+Sans|Playfair+Display|Poppins" rel="stylesheet">
+<link rel="stylesheet" href="./public/style/dach.css">
+<link rel="stylesheet" href="./public/style/form.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+<script src="https://cdn.tailwindcss.com"></script>
+
 </body>
 </html>

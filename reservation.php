540016b8584603f94ba1cc6fa6a2aa6a541dc393
hamzalhadouchi <hamzabniss16@gmail.com>
2024-12-20 22:21:27 +0100

    Query database for reservations and store results.*

--- reservation.php
+++ reservation.php
@@ -1,6 +1,12 @@
 <?php 
 require './db/database.php';
 session_start();
+$result = $connect->query("SELECT * FROM `reservation`");
+if ($result && $result->num_rows > 0) {
+    $reservation = $result->fetch_all(MYSQLI_ASSOC);
+} else {
+    $reservation = [];
+}
 
 
 ?>
